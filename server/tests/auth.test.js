52e9ff348380eef26daf03eed12eb21eeb784e39
Sam <Sam@DESKTOP-GGKIK1S>
2019-06-22 03:50:02 +0100

    checking to see if the jwt  environment variable is failing by ci build

--- server/tests/auth.test.js
+++ server/tests/auth.test.js
@@ -19,20 +19,20 @@ describe('/api/v1/auth', () => {
     it('should return 201 if provide with valid data', async () => {
       expect(user.status).to.equal(201);
     });
-    // it('should return 400 if provide with invalid data', async () => {
-    //   let result = await chai
-    //     .request(app)
-    //     .post('/api/v1/auth/signup')
-    //     .send({ email: 'john', password: 'password' });
-    //   expect(result.status).to.equal(400);
-    // });
-    // it('should return 400 if provided with already existing email', async () => {
-    //   let result = await chai
-    //     .request(app)
-    //     .post('/api/v1/auth/signup')
-    //     .send(validUser);
-    //   expect(result.status).to.equal(400);
-    // });
+    it('should return 400 if provide with invalid data', async () => {
+      let result = await chai
+        .request(app)
+        .post('/api/v1/auth/signup')
+        .send({ email: 'john', password: 'password' });
+      expect(result.status).to.equal(400);
+    });
+    it('should return 400 if provided with already existing email', async () => {
+      let result = await chai
+        .request(app)
+        .post('/api/v1/auth/signup')
+        .send(validUser);
+      expect(result.status).to.equal(400);
+    });
   });
 
   describe('POST /signin', () => {
@@ -44,29 +44,29 @@ describe('/api/v1/auth', () => {
         .send(validLogin);
       expect(result.status).to.equal(200);
     });
-    // it('should return 400 if provided with no values', async () => {
-    //   let result = await chai
-    //     .request(app)
-    //     .post('/api/v1/auth/signin')
-    //     .set('Content-Type', 'application/json')
-    //     .send({});
-    //   expect(result.status).to.equal(400);
-    // });
-    // it('should return 400 if provided with invalid credential', async () => {
-    //   let result = await chai
-    //     .request(app)
-    //     .post('/api/v1/auth/signin')
-    //     .set('Content-Type', 'application/json')
-    //     .send({ email: 'demo@demo.com', password: 'demopassword' });
-    //   expect(result.status).to.equal(400);
-    // });
-    // it('should return 400 if provided with valid email but wrong password', async () => {
-    //   let result = await chai
-    //     .request(app)
-    //     .post('/api/v1/auth/signin')
-    //     .set('Content-Type', 'application/json')
-    //     .send({ ...validLogin, password: 'secret45' });
-    //   expect(result.status).to.equal(400);
-    // });
+    it('should return 400 if provided with no values', async () => {
+      let result = await chai
+        .request(app)
+        .post('/api/v1/auth/signin')
+        .set('Content-Type', 'application/json')
+        .send({});
+      expect(result.status).to.equal(400);
+    });
+    it('should return 400 if provided with invalid credential', async () => {
+      let result = await chai
+        .request(app)
+        .post('/api/v1/auth/signin')
+        .set('Content-Type', 'application/json')
+        .send({ email: 'demo@demo.com', password: 'demopassword' });
+      expect(result.status).to.equal(400);
+    });
+    it('should return 400 if provided with valid email but wrong password', async () => {
+      let result = await chai
+        .request(app)
+        .post('/api/v1/auth/signin')
+        .set('Content-Type', 'application/json')
+        .send({ ...validLogin, password: 'secret45' });
+      expect(result.status).to.equal(400);
+    });
   });
 });
