e8a46a14dc5d40062f9e6d10bd159f7b9a7af632
Sam <Sam@DESKTOP-GGKIK1S>
2019-07-06 17:22:44 +0100

    trying to make travis test db work

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,14 @@
 import { Pool } from 'pg';
 
-const connectionString = process.env.NODE_ENV === 'test' ? process.env.TEST_DB : process.env.DEV_DB;
+let connectionString;
+if (process.env.NODE_ENV === 'test') {
+  connectionString = 'postgres://postgres@127.0.0.1:5432/travis_ci_test';
+} else {
+  connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${
+    process.env.DB_HOST
+  }:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+}
+
 const pool = new Pool({
   connectionString
 });
