f7a89999a51d1a627db46af8d9e3f20bd216ef31
Sam <Sam@DESKTOP-GGKIK1S>
2019-07-08 15:14:57 +0100

    ch-set ui environment - created css, img and js folders inside of ui - created the index page, style and js file
    [finishes #167136189]

---
+++ ui/js/main.js
@@ -0,0 +1,38 @@
+const myMap = address => {
+  let geocoder = new google.maps.Geocoder();
+
+  geocoder.geocode({ address }, (results, status) => {
+    if (status == google.maps.GeocoderStatus.OK) {
+      const { lat, lng } = results[0].geometry.location;
+      let center = new google.maps.LatLng(lat(), lng());
+      let mapProp = {
+        center,
+        zoom: 16,
+        mapTypeId: google.maps.MapTypeId.HYBRID
+      };
+
+      let map = new google.maps.Map(
+        document.getElementById("googleMap"),
+        mapProp
+      );
+
+      let marker = new google.maps.Marker({ position: center });
+
+      marker.setMap(map);
+    }
+  });
+};
+const useModal = (modal, trigerButton, closeButton) => {
+  trigerButton.onclick = e => {
+    e.preventDefault();
+    modal.style.display = "block";
+  };
+
+  closeButton.onclick = e => {
+    modal.style.display = "none";
+  };
+
+  window.onclick = ({ target }) => {
+    if (target == modal) modal.style.display = "none";
+  };
+};
